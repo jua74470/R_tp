9bcf479e8ff3576af058e5df3a2595d3f5ac97dc
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 00:52:31 +0100

    Update client.lua

--- R_Tp/client.lua
+++ R_Tp/client.lua
@@ -1,22 +1,11 @@
 --//////////////////////////////////////////### By RyUk Discord : ryukp2m ###\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-RegisterCommand("tp", function(source, args, rawCommand)
-    local x, y, z = tonumber(args[1]), tonumber(args[2]), tonumber(args[3])
-
-    if x and y and z then
-        TeleportPlayer(vector3(x, y, z))
-        TriggerEvent("chatMessage", "SYSTEM", {255, 0, 0}, "Téléportation effectuée.") -- EN ? Translation : Teleportation completed
-    else
-        TriggerEvent("chatMessage", "SYSTEM", {255, 0, 0}, "Utilisation : /tp x y z") -- EN ? Translation : Use
-    end
-end, false)
-
-function TeleportPlayer(coords)
-    local player = GetPlayerPed(-1)
-
-    if IsPedInAnyVehicle(player, false) then
-        TaskLeaveVehicle(player, GetVehiclePedIsIn(player, false), 0)
-    end
-
-    SetEntityCoordsNoOffset(player, coords.x, coords.y, coords.z, true, true, true)
+RegisterCommand(‘tpto’, function(source, args, rawCommand)
+if tonumber(args[1]) ~= nil and tonumber(args[2]) ~= nil and tonumber(args[3]) ~= nil then
+local posx = tonumber(args[1])
+local posy = tonumber(args[2])
+local posz = tonumber(args[3])
+local coords = { x = posx, y = posy, z = posz}
+ESX.Game.Teleport(PlayerPedId(), coords)
 end
+end)
